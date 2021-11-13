09d3f5a9a7dd3b34d92a7b624d34eda46b135dc1
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-11-13 12:29:20 -0500

    Created folders

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>This is the meal planner
-      </p>
+      <p>This is the meal planner.</p>
     </div>
   );
 }
---
+++ src/Components/Recipe/recipe.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
+
+function Recipe() {
+    return (
+        <div>
+            This is a recipe.
+        </div>
+    )
+}
+
+export default Recipe;
