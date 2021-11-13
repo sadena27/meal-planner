6b6a427a8908071187255be5a99a2bce30a15c63
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-11-13 13:12:51 -0500

    Initial navbar created

---
+++ src/Components/Generator/generator.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+// import axios from 'axios';
+
+function Generator() {
+    return (
+        <div>
+            This is the meal plan generator.
+        </div>
+    )
+}
+
+export default Generator;
