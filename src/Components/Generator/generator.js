2f18423347322125f938ac38dd577253a1a98ccd
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-11-13 20:00:04 -0500

    Updated recipes

--- src/Components/Generator/generator.js
+++ src/Components/Generator/generator.js
@@ -6,19 +6,32 @@ import Recipe from "./recipe";
 import './generator.css';
 
 function Generator() {
-    const [recipes, setRecipes] = useState([{id: '', title: '', image: ''}]);
+    // const [recipes, setRecipes] = useState([{id: '', title: '', image: ''}]);
+    const [recipes, setRecipes] = useState([
+        {
+            "id": 716429,
+            "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+            "image": "https://spoonacular.com/recipeImages/716429-312x231.jpg",
+            "imageType": "jpg",
+        },
+        {
+            "id": 715538,
+            "title": "What to make for dinner tonight?? Bruschetta Style Pork & Pasta",
+            "image": "https://spoonacular.com/recipeImages/715538-312x231.jpg",
+            "imageType": "jpg",
+        }]);
     const api_key = process.env.REACT_APP_API_KEY;
     require('dotenv').config();
 
-    useEffect(() => {
-        async function getRecipes() {
-            const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=' + 
-                                            api_key + '&cuisine=italian&diet=vegetarian');
+    // useEffect(() => {
+    //     async function getRecipes() {
+    //         const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=' + 
+    //                                         api_key + '&cuisine=italian&diet=vegetarian');
             
-            setRecipes(response.data.results);
-        }
-        getRecipes();
-     }, [api_key])
+    //         setRecipes(response.data.results);
+    //     }
+    //     getRecipes();
+    //  }, [api_key])
 
     return (
         <div>
